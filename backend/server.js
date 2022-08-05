cab67ca14f30e20ca25722bc0aaf2ee0ff235368
Max Rifkin <maxrifkin@Maxs-MacBook-Pro.local>
2022-08-05 11:46:26 -0400

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const path = require('path')
 const port = process.env.PORT || 5000  
 const app = express();
 
@@ -13,6 +14,20 @@ app.use(urlencoded({extended: false}))
  
 app.use('/api/reviews', require('./routes/reviewRoutes'))
 app.use(errorHandler);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+  
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
