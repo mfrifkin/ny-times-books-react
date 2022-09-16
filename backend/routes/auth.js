30e03b84515eeb647586daea8b8dd9dcceab5b44
Max Rifkin <maxrifkin@Maxs-MacBook-Pro.local>
2022-09-16 08:53:15 -0400

    login and logout buttons and auth middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
 
 //@desc login with google
 //@route GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+router.get('/google',ensureGuest, passport.authenticate('google', { scope: ['profile'] }))
 
-//@desc google auth callbacl
+//@desc google auth callback
 //@route GET /google/auth/callback
 router.get('/google/callback', passport.authenticate('google',
     { failureRedirect: '/' }), (req, res) => {
@@ -17,7 +17,7 @@ router.get('/google/callback', passport.authenticate('google',
 
 //@desc logout
 //@route GET /google/auth/callback
-router.get('/logout', (req,res,next)=>{
+router.get('/logout',ensureAuth, (req,res,next)=>{
     //passport sets a logout method in the request
     req.logout((error)=>{return next(error)})
     res.redirect('/')
--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
 const {getReviews, postReview, updateReview, deleteReview} = require('../controllers/reviewController')
 
-router.get('/', getReviews)
+router.get('/', ensureAuth, getReviews)
 
 router.post('/', postReview)
 
